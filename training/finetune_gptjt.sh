e4c16103a273e367b2fea6878bad37f5431046a2
Jue Wang <zjuwangjue@gmail.com>
2023-03-04 13:50:46 +0000

    gpt-jt

---
+++ training/finetune_gptjt.sh
@@ -0,0 +1,49 @@
+netif=lo
+export GLOO_SOCKET_IFNAME=${netif}
+export NCCL_SOCKET_IFNAME=${netif}
+export WANDB_NAME=gptjt-train
+
+export SHOW_DATA=0
+
+PATH_TO_MODEL="{{PATH_TO_MODEL}}"
+
+ARGS="--model-name ${PATH_TO_MODEL} \
+--tokenizer-name ${PATH_TO_MODEL} \
+--project-name together \
+--model-type gptj \
+--optimizer adam \
+--seed 42 \
+--load-pretrained-model true \
+--task-name {{PATH_TO_DATA_0}} \
+--checkpoint-path ./model_ckpts/$WANDB_NAME \
+--total-steps 20000 --warmup-steps 10 --train-warmup-steps 0 \
+--checkpoint-steps 100 \
+--lr 1e-5 --seq-length 2048 --batch-size 16 --micro-batch-size 1 --gradient-accumulate-step 1 \
+--dist-url tcp://127.0.0.1:7033 \
+--num-layers 14 --embedding-dim 4096 \
+--world-size 8 --pipeline-group-size 2 --data-group-size 4 \
+--job-id 0 --net-interface ${netif} \
+--fp16 \
+--dp-backend nccl \
+--dp-mode allreduce \
+--pp-mode gpipe --profiling no-profiling"
+
+(trap 'kill 0' SIGINT; \
+python dist_prefixlm_train.py $(echo ${ARGS}) --cuda-id 0 --rank 0 \
+    & \
+python dist_prefixlm_train.py $(echo ${ARGS}) --cuda-id 1 --rank 1 \
+    & \
+python dist_prefixlm_train.py $(echo ${ARGS}) --cuda-id 2 --rank 2 \
+    & \
+python dist_prefixlm_train.py $(echo ${ARGS}) --cuda-id 3 --rank 3 \
+    & \
+python dist_prefixlm_train.py $(echo ${ARGS}) --cuda-id 4 --rank 4 \
+    & \
+python dist_prefixlm_train.py $(echo ${ARGS}) --cuda-id 5 --rank 5 \
+    & \
+python dist_prefixlm_train.py $(echo ${ARGS}) --cuda-id 6 --rank 6 \
+    & \
+python dist_prefixlm_train.py $(echo ${ARGS}) --cuda-id 7 --rank 7 \
+    & \
+wait)
+
--- training/tasks/data_loaders/data_utils.py
+++ training/tasks/data_loaders/data_utils.py
@@ -15,6 +15,7 @@ from itertools import islice
 from random import randint
 
 SHOW_DATA = int(os.environ.get('SHOW_DATA', 1))
+UL2R_DENOISE_ENABLED = int(os.environ.get('UL2R_DENOISE_ENABLED', 0))
 
 
 import os
@@ -26,6 +27,149 @@ import random
 from datasets import Dataset
 from datasets import load_dataset, load_from_disk
 from comm.comm_utils import *
+
+
+def random_chunk(li, min_chunk=1, max_chunk=5):
+    it = iter(li)
+    while True:
+        nxt = list(islice(it,randint(min_chunk,max_chunk)))
+        if nxt:
+            yield nxt
+        else:
+            break
+            
+
+class UL2RProcessor:
+    '''
+    This is a replication of UL2R from our understanding.
+    We welcome PR if there are better implementations.
+    '''
+    
+    def __init__(self, tokenizer, seq_length=1024):
+        self.tokenizer = tokenizer
+        self.seq_length = seq_length
+        
+        self.s2s_prefix = self.tokenizer("[S2S]")['input_ids']
+        self.nlg_prefix = self.tokenizer("[NLG]")['input_ids']
+        self.nlu_prefix = self.tokenizer("[NLU]")['input_ids']
+        
+        self.extra_ids = [self.tokenizer.eos_token_id - 100 + i for i in range(80)]
+        
+        
+    def preprocess_tokens_s2s(self, tokens):
+        
+        tokens = self.s2s_prefix + tokens
+        
+        split = int(random.random() * len(tokens))
+        
+        tokens = tokens[:split] + tokens[split:]
+        tokens = tokens[:self.seq_length]
+        
+        prefix_masks = torch.zeros(len(tokens), dtype=torch.uint8)
+        prefix_masks[:split] = 1
+        
+        return {
+            'input_ids': torch.tensor(tokens),
+            'prefix_masks': prefix_masks,
+        }
+    
+    def preprocess_tokens_nlg(self, tokens):
+        
+        tokens = tokens[:self.seq_length - len(self.nlg_prefix) - 2]
+        
+        start = int(random.random() * len(tokens))
+        end = start + 1 + int(random.random() * 31)
+        
+        left = self.nlg_prefix + tokens[:start] + [self.extra_ids[0]] + tokens[end:]
+        right = [self.extra_ids[0]] + tokens[start:end]
+    
+        tokens = left + right
+        tokens = tokens[:self.seq_length]
+        tokens = tokens + (self.seq_length - len(tokens)) * [self.tokenizer.eos_token_id]
+        
+        prefix_masks = torch.zeros(len(tokens), dtype=torch.uint8)
+        prefix_masks[:len(left)] = 1
+        
+        return {
+            'input_ids': torch.tensor(tokens),
+            'prefix_masks': prefix_masks,
+        }
+        
+    def preprocess_tokens_nlu(self, tokens):
+        
+        tokens = tokens[:self.seq_length - len(self.nlu_prefix) - 10]
+        
+        # split to chunks
+        chunks = list(random_chunk(tokens, min_chunk=1, max_chunk=5))
+        
+        # randomly select 15%
+        K = int(0.15 * len(chunks))
+        indices = random.sample(range(len(chunks)), K)
+        
+        left = self.nlu_prefix
+        right = []
+        extra_id_count = 0
+        
+        last_corrupt = False
+        for i, chunk in enumerate(chunks):
+            # make sure not consecutive corrupt chunks
+            if i in indices and not last_corrupt and extra_id_count < len(self.extra_ids):
+                left += [self.extra_ids[extra_id_count]]
+                right += [self.extra_ids[extra_id_count]] + chunk
+                extra_id_count += 1
+            else:
+                left += chunk
+                last_corrupt = False
+        
+        tokens = left + right
+        tokens = tokens[:self.seq_length]
+        tokens = tokens + (self.seq_length - len(tokens)) * [self.tokenizer.eos_token_id]
+        
+        prefix_masks = torch.zeros(len(tokens), dtype=torch.uint8)
+        prefix_masks[:len(left)] = 1
+        
+        return {
+            'input_ids': torch.tensor(tokens),
+            'prefix_masks': prefix_masks,
+        }
+
+    def preprocess_ul2r(self, inputs):
+        tokens = inputs['input_ids'].tolist()
+        p = random.random()
+        if p > 0.5:
+            return self.preprocess_tokens_s2s(tokens)
+        elif p > 0.25:
+            return self.preprocess_tokens_nlg(tokens)
+        else:
+            return self.preprocess_tokens_nlu(tokens)
+    
+    def preprocess_random(self, inputs):
+        
+        tokens = inputs['input_ids'].tolist()
+        
+        if random.random() < 0.2:
+            # short prompt
+            split = int(random.random() * 20)
+        else:
+            # random length prompt
+            split = int(random.random() * len(tokens))
+        
+        tokens = tokens[:split] + tokens[split:]
+        tokens = tokens[:self.seq_length]
+        
+        prefix_masks = torch.zeros(len(tokens), dtype=torch.uint8)
+        prefix_masks[:split] = 1
+        
+        return {
+            'input_ids': torch.tensor(tokens),
+            'prefix_masks': prefix_masks,
+        }
+    
+    def __call__(self, inputs):
+        if UL2R_DENOISE_ENABLED:
+            return self.preprocess_ul2r(inputs)
+        else:
+            return self.preprocess_random(inputs)
 
 
 class StreamDataset(IterableDataset):
@@ -216,3 +360,43 @@ def get_eval_data_loader(args, tokenizer, num_workers=1, state_dict=None):
     
     return train_data_loader
 
+
+def get_ul2r_train_data_loader(args, tokenizer, num_workers=1, state_dict=None):
+    
+    task_list = args.task_name.split(',')
+    task_names = []
+    datasets = []
+    probs = []
+    for task in task_list:
+        if ':' in task:
+            task, prob = task.strip().split(':')
+            prob = float(prob)
+        else:
+            task = task.strip()
+            prob = 1.0
+            
+        dataset = name_to_dataset(task, tokenizer, args)
+    
+        task_names.append(task)
+        datasets.append(dataset)
+        probs.append(prob)
+        
+    ul2r_processor = UL2RProcessor(tokenizer, seq_length=args.seq_length)
+    
+    stream_dataset = StreamDatasetList(
+        task_names, datasets, probs,
+        tokenizer=tokenizer, seq_length=args.seq_length, post_processor=ul2r_processor)
+    
+    if state_dict is not None:
+        stream_dataset.load_state_dict(state_dict)
+    
+    train_data_loader = torch.utils.data.DataLoader(stream_dataset,
+                                                    batch_size=args.batch_size * args.data_group_size,
+                                                    shuffle=False,
+                                                    num_workers=num_workers,
+                                                    pin_memory=True,
+                                                    collate_fn=None)
+    
+    print('ul2r dataloader init done.')
+    
+    return train_data_loader
