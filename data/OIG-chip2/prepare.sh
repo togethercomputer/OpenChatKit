35b04aa79376ed89428a00de6b4e676bba2801bc
Xiaozhe Yao <xiaozhe.yaoi@gmail.com>
2023-05-07 23:54:32 +0200

    Update data/OIG-chip2/prepare.sh
    Co-authored-by: Charles Srisuwananukorn <1967608+csris@users.noreply.github.com>

--- data/OIG-chip2/prepare.sh
+++ data/OIG-chip2/prepare.sh
@@ -1,1 +1,3 @@
-wget https://huggingface.co/datasets/laion/OIG/resolve/main/unified_chip2.jsonl -O data/OIG-chip2/unified_chip2.jsonl
+DIR=$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
+
+wget https://huggingface.co/datasets/laion/OIG/resolve/main/unified_chip2.jsonl -O ${DIR}/data/OIG-chip2/unified_chip2.jsonl
