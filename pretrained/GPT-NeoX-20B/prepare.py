81d2e4357db88d4f470b8e066d3149a138a1461e
Justus Calvin <Together>
2023-05-16 10:25:57 -0700

    Reorganize pretrained model prepare scripts

--- pretrained/GPT-NeoX-20B/prepare.py
+++ pretrained/GPT-NeoX-20B/prepare.py
@@ -1,3 +1,15 @@
+import sys
+import os
+
+# Import the prepare_data function
+current_dir = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.join(current_dir, '..'))
+from prepare_pretrained import prepare_pretrained
+
+if __name__ == "__main__":
+    prepare_pretrained(current_dir, "EleutherAI/gpt-neox-20b")
+
+
 import os
 import argparse
 import torch
