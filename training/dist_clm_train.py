6c8f0811a8ef39bb368e491db7fe720c0baca789
orangetin <126978607+orangetin@users.noreply.github.com>
2023-06-26 17:38:11 -0700

    minor fix

--- training/dist_clm_train.py
+++ training/dist_clm_train.py
@@ -146,7 +146,7 @@ def train_loop(args, pipe, device, train_data_loader, test_data_loader):
             labels = input_ids.clone()
             current_iter_time = pipe.sgd_iter(input_ids, labels, loss_func=gpt_loss_func)
 
-            if event_reporter is not None and epoch_steps != 0 and pipe.global_step % args.epoch_steps == 0:
+            if event_reporter is not None and epoch_steps != 0 and pipe.global_step % epoch_steps == 0:
                 event_reporter.report(object=EventReporter.OBJECT_FINE_TUNE,
                                       message=f"Epoch competed for step {pipe.global_step}",
                                       event_type=EventReporter.EVENT_TYPE_EPOCH_COMPLETE,
