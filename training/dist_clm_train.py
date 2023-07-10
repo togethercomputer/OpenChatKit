7e5b4903fb1a27b1e66426e3b49aff9a695a3973
Justus Calvin <justus@together.xyz>
2023-07-10 11:22:22 -0700

    Implement checkpoint upload to S3

--- training/dist_clm_train.py
+++ training/dist_clm_train.py
@@ -18,6 +18,8 @@ from utils.logging_utils import *
 from utils.event_report import *
 from comm.comm_utils import *
 
+from utils.upload_manager import *
+
 
 def test_loop(args, pipe, device, test_data_loader):
     
@@ -106,6 +108,16 @@ def train_loop(args, pipe, device, train_data_loader, test_data_loader):
 
     if get_pipeline_parallel_rank() == 0 and dp_rank == 0:
 
+        upload_manager = None
+        if args.upload_checkpoints:
+            upload_manager = UploadManager(aws_endpoint_url = args.aws_endpoint_url,
+                                           aws_access_key_id = args.aws_access_key_id,
+                                           aws_secret_access_key = args.aws_secret_access_key,
+                                           aws_session_token = args.aws_session_token,
+                                           aws_region = args.aws_region,
+                                           event_reporter = event_reporter,
+                                           n_stages = dp_size)
+
         epoch_steps = int(train_data_loader.dataset.get_dataset_example_count() / args.batch_size)
         if epoch_steps == 0:
             epoch_steps = 1
@@ -164,7 +176,9 @@ def train_loop(args, pipe, device, train_data_loader, test_data_loader):
                 if do_sync_before_save:
                     pipe.dp_optim.allreduce_parameters()
                 if dp_rank == 0:
-                    save_checkpoint(pipe, args)
+                    checkpoint_step_path = save_checkpoint(pipe, args)
+                    if args.upload_checkpoints:
+                        upload_manager.upload_checkpoints(checkpoint_step_path, args.bucket_name, args.prefix, pipe.global_step)
 
                 if do_sync_before_save:
                     pipe.dp_optim.rollback_parameters()
@@ -294,7 +308,15 @@ def main():
     parser.add_argument('--job-id', 
                         type=str, default="0", metavar='S',
                         help='an uuid')
+    
+    # Add AWS arguments for uploading checkpoints to S3
+    parser.add_argument('--bucket-name', required=True, help='S3 bucket name')
+    parser.add_argument('--prefix', required=True, help='Prefix for the S3 objects')
+    parser.add_argument('--upload-checkpoints', action='store_true', default=False, help='Upload checkpoints to S3')
+    add_aws_arguments(parser)
+
     args = parser.parse_args()
+    aws_process_args(args)
     
     torch.manual_seed(args.seed)
     random.seed(args.seed)
